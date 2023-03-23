7fa5682e5f1bbe5d949ea6ce3e97649181541cf4
chengzhinei <chengxue5yuan@163.com>
2023-03-23 10:47:03 +0800

    add log

--- libs/tendermint/state/execution_dds.go
+++ libs/tendermint/state/execution_dds.go
@@ -306,6 +306,8 @@ func (dc *DeltaContext) prepareStateDelta(height int64) *DeltaInfo {
 		return nil
 	}
 
+	time.Sleep(time.Second)
+
 	deltaInfo, mrh := dc.dataMap.fetch(height)
 
 	atomic.StoreInt64(&dc.lastFetchedHeight, height)
